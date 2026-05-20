54d65bba5aee5788bc48556e27ff42bd0cfecd0b
V01D <aeternal.infinity@gmail.com>
2026-05-20 14:29:34 +0300

    AUTHOR

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
-def print_author():
-    author = os.getenv(AUTHOR)
-    print (fАвтор проекта: {author})
+> from dotenv import load_dotenv
+> load_dotenv()
+> def print_author():
+>     author = os.getenv("AUTHOR")
+>     print (f"Автор проекта: {author}")
