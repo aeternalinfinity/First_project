e43ece105c5abab644bdeec49a8f568584d51bf2
V01D <aeternal.infinity@gmail.com>
2026-05-20 14:31:39 +0300

    AUTHOR

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import os
-> from dotenv import load_dotenv
-> load_dotenv()
-> def print_author():
->     author = os.getenv("AUTHOR")
->     print (f"Автор проекта: {author}")
+from dotenv import load_dotenv
+
+# Загружаем переменные окружения из файла .env
+load_dotenv()
+
+def print_author():
+    # Считываем значение переменной AUTHOR
+    author = os.getenv("AUTHOR")
+    
+    print(f"Автор проекта: {author}")
+
+# Вызов функции для проверки работы
+if __name__ == "__main__":
+    print_author()
